8c979e6b8e1d8b0be025cd2b24c415a17e1e1e28
Evelina Berg <evelina.berg@outlook.com>
2020-11-19 22:44:19 +0100

    Unit tests completed

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,10 @@
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
-
+  console.log(type);
   if (type === 'string') {
     return stringArrayOrObject === ''
+  } else if (type === 'object') {
+    return Object.keys(stringArrayOrObject).length === 0;
   }
-
   return false
 }
